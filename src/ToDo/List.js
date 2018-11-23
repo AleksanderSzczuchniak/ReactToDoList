e22a39944de8534c2d93693b524689d559d39359
AleksanderSzczuchniak <alex.szczuchniak@gmail.com>
2018-11-23 14:44:35 +0100

    dodatkowa opcja do filtra ignorująca znaki

--- src/ToDo/List.js
+++ src/ToDo/List.js
@@ -1,36 +1,45 @@
 import React from 'react'
+
+import { unifyString } from '../utils'
+
 import Task from './Task'
 
 const List = (props) => (
-    <div>
-        {
-            props.tasksList &&
-            props.tasksList.map &&
-            props.tasksList
-            .filter( task => {
-                switch(props.chosenFilter){
-                    case 'ALL':
-                        return true
-                    case 'COMPLETED':
-                        return task.isCompleted
-                    case 'UNCOMPLETED':
-                        return  !task.isCompleted
-                    default:
-                        return true
-
-                }
-            })
-            .map(
-                task => (
-                    <Task
-                        key={task.key}
-                        task={task}
-                        completeTask={props.completeTask}
-                        deleteTask={props.deleteTask}
-                    />
-                )
+  <div>
+    {
+      props.tasksList &&
+      props.tasksList.map &&
+      props.tasksList
+        .filter(task => {
+          switch (props.chosenFilter) {
+            case 'ALL':
+              return true
+            case 'COMPLETED':
+              return task.isCompleted
+            case 'UNCOMPLETED':
+              return !task.isCompleted
+            default:
+              return true
+          }
+        })
+        .filter(task => (
+          unifyString(task.taskText)
+            .includes(
+              unifyString(props.filterText)
             )
-        }
-    </div>
+        ))
+        .map(
+          task => (
+            <Task
+              key={task.key}
+              task={task}
+              completeTask={props.completeTask}
+              deleteTask={props.deleteTask}
+            />
+          )
+        )
+    }
+  </div>
 )
+
 export default List
