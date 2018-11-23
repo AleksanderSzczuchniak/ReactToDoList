f84953d6fec041caf7dca7ebbbcd352bced9e56d
AleksanderSzczuchniak <alex.szczuchniak@gmail.com>
2018-11-23 10:56:20 +0100

    funkcje w logice

--- src/ToDo/ToDo.js
+++ src/ToDo/ToDo.js
@@ -8,6 +8,39 @@ class ToDo extends React.Component {
         chosenFilter: 'ALL',
         newTaskText: ''
     }
+
+    createTask = text => ({
+        taskText: text,
+        isCompleted: false,
+        key: Date.now()
+    })
+
+    addTask = () => this.setState({
+        tasks: this.setState.tasks.concat(
+            this.createTask(
+                this.state.newTaskText
+            )
+        )
+    })
+    deleteTask = taskKey => this.setState({
+        task:this.state.task.filter(
+            task => task.key !== taskKey
+        )
+    })
+    completeTask = taskKey => this.setState({
+        task: this.state.task.map(
+            task => (
+                (task.key === taskKey) ?
+                {
+                    ...task,
+                    isCompleted: true
+                }
+                :
+                task
+            )
+        )
+    })
+
     render(){
         return(
             <div>
